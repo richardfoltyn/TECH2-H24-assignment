252582ecc001b97599cb3f956a39d86e431e5ce4
Richard Foltyn <richard.foltyn@gmail.com>
2024-09-26 15:14:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TECH2 mandatory assignment
+
+The assignment is given in the in the PDF file [assignment.pdf](assignment.pdf).
+
+To write your solution, you should
+
+1. Fork this repository
+2. Add your implementation to `part_A.py` and `part_B.ipynb.`
+3. Make sure the repository containing the solution is public!
+
+Deadline: Sunday, October 6, 2024 at 23:59
---
+++ part_A.py
@@ -0,0 +1,35 @@
+"""
+TECH2 mandatory assignment - Part A
+
+Write the implementation of part A of the exercise below.
+"""
+
+def std_loops(x):
+    """
+    Compute standard deviation of x using loops.
+
+    Parameters
+    ----------
+    x: Sequence of numbers
+
+    Returns
+    -------
+    sd : float
+        Standard deviation of the list of numbers.
+    """
+
+def std_builtin(x):
+    """
+    Compute standard deviation of x using the built-in functions sum()
+    and len().
+
+    Parameters
+    ----------
+    x: Sequence of numbers
+
+    Returns
+    -------
+    sd : float
+        Standard deviation of the list of numbers.
+    """
+    
